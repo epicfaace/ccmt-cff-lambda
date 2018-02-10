337c27c8d562606d82b37331f867b72c54cb6ec2
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-02-10 13:27:53 -0800

    format currency with two decimal places in email

--- functions/forms/lib/util.py
+++ functions/forms/lib/util.py
@@ -145,8 +145,8 @@ def calculate_price(expressionString, data):
 
 def format_payment(total, currency='USD'):
     if currency == "USD":
-        return "${}".format(total)
-    return "{} {}".format(currency, total)
+        return "${:,.2f}".format(total)
+    return "{} {:,.2f}".format(currency, total)
 def format_paymentInfo(paymentInfo):
     return format_payment(paymentInfo.get("total", "N/A"), paymentInfo.get("currency", "USD"))
 
