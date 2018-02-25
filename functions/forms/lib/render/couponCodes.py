839d82c58ba544f0b0dc3e526d68502baf5a35ec
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-02-25 14:50:28 -0800

    Bugfix: allow coupon codes with arbitrary names; and allow maximum to be negative (no maximum)

--- functions/forms/lib/render/couponCodes.py
+++ functions/forms/lib/render/couponCodes.py
@@ -1,35 +1,39 @@
 def coupon_code_verify_max(form, code, responseId=None):
     # True: coupon code can be used (either length of coupon codes used is not at max, or your ID has already used the coupon code before.)
+    # If maximum is negative, that means there is no maximum.
     responses = form.get("couponCodes_used", {}).get(code, {}).get("responses", [])
-    maximum = form.get("couponCodes", {}).get(code, {}).get("max", 0)
-    return responseId in responses or len(responses) < maximum
+    maximum = form.get("couponCodes", {}).get(code, {}).get("max", -1)
+    return responseId in responses or maximum < 0 or len(responses) < maximum
 
 def coupon_code_record_as_used(formsCollection, form, code, responseId):
     # form = formsCollection.get_item(Key=formKey)["Item"]
     # code = "TEST"
     # responseId = "123123-123123"
     formKey = {"id": form['id'], "version": int(form['version'])}
-    path = "couponCodes_used.{}.responses".format(code)
     if "couponCodes_used" in form:
         if code in form["couponCodes_used"] and "responses" in form["couponCodes_used"][code]:
             if responseId in form["couponCodes_used"][code]["responses"]:
                 return True
             else:
-                path = "couponCodes_used.{}.responses".format(code)
                 formsCollection.update_item(
                     Key = formKey,
-                    UpdateExpression="SET {0} = list_append(if_not_exists({0}, :empty_list), :responseId)".format(path),
+                    UpdateExpression="SET couponCodes_used.#code.responses = list_append(if_not_exists(couponCodes_used.#code.responses, :empty_list), :responseId)",
+                    ExpressionAttributeNames={
+                        "#code": code
+                    },
                     ExpressionAttributeValues = {
                         ":responseId": [responseId],
                         ":empty_list": []
                     },
-                    ConditionExpression="attribute_exists({})".format(path)
+                    ConditionExpression="attribute_exists(couponCodes_used.#code.responses)"
                 )
         else:
-            path = "couponCodes_used.{}".format(code)
             formsCollection.update_item(
                 Key = formKey,
-                UpdateExpression="SET {0} = :couponCodeValue".format(path),
+                UpdateExpression="SET couponCodes_used.#code = :couponCodeValue",
+                ExpressionAttributeNames={
+                    "#code": code
+                },
                 ExpressionAttributeValues = {
                     ":couponCodeValue": {"responses": [responseId] }
                 }
